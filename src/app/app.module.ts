926d2b70beced19d04df111c81ea4d599171632c
Fadi Fannoun <faditamimi@gmail.com>
2022-02-23 16:47:42 +0200

    added service worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { TodayComponent } from './pages/today/today.component';
 import { PlacesAutocompleteDirective } from './directives/places-autocomplete.directive';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,13 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     AppRoutingModule,
 	HttpClientModule,
 	FormsModule,
-	ReactiveFormsModule
+	ReactiveFormsModule,
+ 	ServiceWorkerModule.register('ngsw-worker.js', {
+   		enabled: environment.production,
+   		// Register the ServiceWorker as soon as the app is stable
+   		// or after 30 seconds (whichever comes first).
+   		registrationStrategy: 'registerWhenStable:30000'
+	})
   ],
   providers: [],
   bootstrap: [AppComponent]
